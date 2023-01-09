267f18cd63931a42254eccfba5264b7f110aafdd
ainur888 <ainurrahman820@gmail.com>
2023-01-09 22:36:43 +0600

    done

---
+++ resource/js/main.js
@@ -0,0 +1,51 @@
+
+            // MIXITUP 
+    var mixer = mixitup('.container');
+
+           // WAYPOINS 
+
+
+
+
+    $('.js-sticky').waypoint(function(direction){
+        if (direction == "down") {
+            $(nav).addClass("sticky");
+        }
+        
+    });
+
+
+    function openNav() {
+        document.getElementById("myNav").style.width="100%";
+    }
+    function closeNav() {
+        document.getElementById("myNav").style.width="0%";
+    }
+
+    
+
+    
+        
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
